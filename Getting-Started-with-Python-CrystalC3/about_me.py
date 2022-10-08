acc499235e8cbf7065ef9277d7c49ff4fa8b2431
CrystalC1 <109238702+CrystalC1@users.noreply.github.com>
2022-10-08 00:47:14 -0700

    completing first python activity files

---
+++ Getting-Started-with-Python-CrystalC3/about_me.py
@@ -0,0 +1,10 @@
+Full_Name = 'Crystal Chagoya'
+Preffered_Name = 'My current name'
+Age = 16
+Have_you_used_python_before = True
+Favorite_hobbies = ['drawing','reading','watching shows']
+favorite_things = {'show': "Infinity train",
+                 'food': 'steak',
+                 'hobby': 'painting'}
+
+all_vars = dict(vars())
